58d0d2d321ba7a4c5f4f737c99ccf96c149eca00
deepuSproudKnowledge <sproudknowledge@gmail.com>
2022-04-25 18:16:02 -0700

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -11,6 +11,34 @@ const routes: Routes = [
     redirectTo: 'home',
     pathMatch: 'full'
   },
+  {
+    path: 'main',
+    loadChildren: () => import('./main/main.module').then( m => m.MainPageModule)
+  },
+  {
+    path: 'regnlogin',
+    loadChildren: () => import('./regnlogin/regnlogin.module').then( m => m.RegnloginPageModule)
+  },
+  {
+    path: 'profile',
+    loadChildren: () => import('./profile/profile.module').then( m => m.ProfilePageModule)
+  },
+  {
+    path: 'chat',
+    loadChildren: () => import('./chat/chat.module').then( m => m.ChatPageModule)
+  },
+  {
+    path: 'status',
+    loadChildren: () => import('./status/status.module').then( m => m.StatusPageModule)
+  },
+  {
+    path: 'videocall',
+    loadChildren: () => import('./videocall/videocall.module').then( m => m.VideocallPageModule)
+  },
+  {
+    path: 'audiocall',
+    loadChildren: () => import('./audiocall/audiocall.module').then( m => m.AudiocallPageModule)
+  },
 ];
 
 @NgModule({
---
+++ src/app/regnlogin/regnlogin.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-regnlogin',
+  templateUrl: './regnlogin.page.html',
+  styleUrls: ['./regnlogin.page.scss'],
+})
+export class RegnloginPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
